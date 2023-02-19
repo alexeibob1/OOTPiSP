91c2dcb23f2247b9f9ea246db9e65d9aa5ae91e7
Alexei Bobrik <fingerloom2004@gmail.com>
2023-02-19 23:36:29 +0300

    feat: create class hierarchy and complete UML diagram

---
+++ ClassHierarchy/ClassHierarchy/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Schedule
+{
+    String departure;
+    String destination;
+    String departureTime;
+    String destinationTime;
+}
+
+public class RailTransport
+{
+    int id;
+    Schedule schedule;
+    int totalPlaces;
+    int maxSpeed;
+    int totalCarriages;
+}
+
+public class ElectricTrain : RailTransport
+{
+    String powerSupply;
+    int voltage;
+}
+
+enum Fuel
+{
+    DIESEL,
+    CARBON,
+    BIOMASS,
+    PETROLEUM
+}
+
+public class DieselTrain : RailTransport
+{
+    Fuel fuel;
+}
+
+public class Subway : ElectricTrain
+{
+    int depth;
+}
+
+public class Tram : ElectricTrain
+{
+    int noisiness;
+}
