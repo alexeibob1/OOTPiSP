74ce501f77a55c10c2f5cb95bbc43e666807fecc
Alexei Bobrik <fingerloom2004@gmail.com>
2023-04-30 00:29:51 +0300

    feat: refactor text serializer

--- src/main/java/com/lab2/serializers/TextSerializer.java
+++ src/main/java/com/lab2/serializers/TextSerializer.java
@@ -4,14 +4,10 @@ import com.lab2.ErrorWindow;
 import com.lab2.trains.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,7 +36,6 @@ public class TextSerializer implements Serializable {
                 writer.println("INNER: " + fieldName);
                 serializeObject(field.get(o), writer, true);
             } else {
-                //writer.println("TYPE: " + fieldType);
                 writer.println("NAME: " + fieldName);
                 writer.println("VALUE: " + field.get(o));
             }
@@ -62,9 +57,7 @@ public class TextSerializer implements Serializable {
             ErrorWindow alert = new ErrorWindow();
             alert.showError(Alert.AlertType.ERROR, "Error!", "Error while text serialization", "During serialization to txt an error occurred. Please, try again");
         }
-
     }
-
 
     public void removeTransientFields(List<Field> fields) {
         for (Field field : fields) {
@@ -73,6 +66,19 @@ public class TextSerializer implements Serializable {
                 break;
             }
         }
+    }
+
+    public Field getHierarchyField(Class<?> objClass, String fieldName) {
+        Field field = null;
+        Class<?> parentClass = objClass;
+        while (parentClass != null) {
+            try {
+                field = parentClass.getDeclaredField(fieldName);
+            } catch (Exception ignored) {
+            }
+            parentClass = parentClass.getSuperclass();
+        }
+        return field;
     }
     @Override
     public ObservableList<RailTransport> deserialize(InputStream inputStream) {
@@ -109,94 +115,38 @@ public class TextSerializer implements Serializable {
                 }
                 else if (state == 2) {
                     if (prevState == 3) {
-                        fieldValue = line.substring(line.lastIndexOf("VALUE:") + 6).trim();
-                        String setMethodName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-
-                        Field field = null;
-                        Class<?> parentClass = trainClass;
-                        while (parentClass != null) {
-                            try {
-                                field = parentClass.getDeclaredField(fieldName);
-                            } catch (Exception e) {
-                            }
-                            parentClass = parentClass.getSuperclass();
-                        }
-
-
+                        fieldValue = extractParam(line, "VALUE:");
+                        Field field = getHierarchyField(trainClass, fieldName);
                         field.setAccessible(true);
-                        String fieldTypeName = field.getType().getName().toLowerCase();
-                        if (fieldTypeName.contains("int")) {
-                            int intVal = Integer.parseInt(fieldValue);
-                            Method setMethod = trainClass.getMethod(setMethodName, int.class);
-                            setMethod.invoke(train, intVal);
-                        }
-                        else if (fieldTypeName.contains("string")) {
-                            Method setMethod = trainClass.getMethod(setMethodName, String.class);
-                            setMethod.invoke(train, fieldValue);
-                        }
-                        else if (fieldTypeName.contains("powersupply")) {
-                            PowerSupply powerSupply = PowerSupply.valueOf(fieldValue);
-                            Method setMethod = trainClass.getMethod(setMethodName, PowerSupply.class);
-                            setMethod.invoke(train, powerSupply);
-                        }
-                        else if (fieldTypeName.contains("fuel")) {
-                            Fuel fuel = Fuel.valueOf(fieldValue);
-                            Method setMethod = trainClass.getMethod(setMethodName, Fuel.class);
-                            setMethod.invoke(train, fuel);
-                        }
+                        invokeSetMethod(trainClass, field, train, fieldValue);
                     }
                     else if (prevState == 1) {
-                        trainClassName = line.substring(line.lastIndexOf("TRAIN:") + 6).trim();
+                        trainClassName = extractParam(line, "TRAIN:");
                         trainClass = Class.forName(trainClassName);
                         Constructor<?> constructor = trainClass.getConstructor();
                         train = constructor.newInstance();
                     }
                     else if (prevState == 4) {
-                        String setMethodName = "set" + innerFieldName.substring(0, 1).toUpperCase() + innerFieldName.substring(1);
+                        String setMethodName = getSetMethodName(innerFieldName);
                         Method setMethod = trainClass.getMethod(setMethodName, innerClass);
                         setMethod.invoke(train, innerObject);
                     }
                 }
                 else if (state == 3 || state == 5) {
-                    fieldName = line.substring(line.lastIndexOf("NAME:") + 5).trim();
+                    fieldName = extractParam(line, "NAME:");
                 }
                 else if (state == 4) {
                     if (prevState == 2) {
-                        innerFieldName = line.substring(line.lastIndexOf("INNER:") + 6).trim();
-                        //Field field = trainClass.getField(innerFieldName);
-
-                        Field field = null;
-                        Class<?> parentClass = trainClass;
-                        while (parentClass != null) {
-                            try {
-                                field = parentClass.getDeclaredField(innerFieldName);
-                            } catch (Exception e) {
-                            }
-                            parentClass = parentClass.getSuperclass();
-                        }
-
+                        innerFieldName = extractParam(line, "INNER:");
+                        Field field = getHierarchyField(trainClass, innerFieldName);
                         innerClass = field.getType();
                         Constructor<?> constructor = innerClass.getConstructor();
                         innerObject = constructor.newInstance();
                     } else if (prevState == 5) {
-                        fieldValue = line.substring(line.lastIndexOf("VALUE:") + 6).trim();
-                        String setMethodName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                        Field field = innerClass.getDeclaredField(fieldName);
+                        fieldValue = extractParam(line, "VALUE:");
+                        Field field = getHierarchyField(innerClass, fieldName);
                         field.setAccessible(true);
-                        Class<?> fieldType = field.getType();
-                        String fieldTypeName = fieldType.getName().toLowerCase();
-                        if (fieldTypeName.contains("int")) {
-                            int intVal = Integer.parseInt(fieldValue);
-                            Method setMethod = innerClass.getMethod(setMethodName, int.class);
-                            setMethod.invoke(innerObject, intVal);
-                        } else if (fieldTypeName.contains("string")) {
-                            Method setMethod = innerClass.getMethod(setMethodName, String.class);
-                            setMethod.invoke(innerObject, fieldValue);
-                        } else if (fieldTypeName.contains("localdate")) {
-                            Method setMethod = innerClass.getMethod(setMethodName, LocalDate.class);
-                            LocalDate date = LocalDate.parse(fieldValue);
-                            setMethod.invoke(innerObject, date);
-                        }
+                        invokeSetMethod(innerClass, field, innerObject, fieldValue);
                     }
                 }
             }
@@ -209,6 +159,48 @@ public class TextSerializer implements Serializable {
         }
 
         return FXCollections.observableArrayList(trains);
+    }
+
+    public String extractParam(String line, String paramName) {
+        return line.substring(line.lastIndexOf(paramName) + paramName.length()).trim();
+    }
+
+    public void invokeSetMethod(Class<?> objClass, Field field, Object obj, String fieldValue) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String fieldName = field.getName();
+        String fieldTypeName = field.getType().getName().toLowerCase();
+        String setMethodName = getSetMethodName(fieldName);
+        if (fieldTypeName.contains("int")) {
+            int intVal = Integer.parseInt(fieldValue);
+            Method setMethod = getSetter(setMethodName, int.class, objClass);
+            setMethod.invoke(obj, intVal);
+        }
+        else if (fieldTypeName.contains("string")) {
+            Method setMethod = getSetter(setMethodName, String.class, objClass);
+            setMethod.invoke(obj, fieldValue);
+        }
+        else if (fieldTypeName.contains("powersupply")) {
+            PowerSupply powerSupply = PowerSupply.valueOf(fieldValue);
+            Method setMethod = getSetter(setMethodName, PowerSupply.class, objClass);
+            setMethod.invoke(obj, powerSupply);
+        }
+        else if (fieldTypeName.contains("fuel")) {
+            Fuel fuel = Fuel.valueOf(fieldValue);
+            Method setMethod = getSetter(setMethodName, Fuel.class, objClass);
+            setMethod.invoke(obj, fuel);
+        }
+        else if (fieldTypeName.contains("localdate")) {
+            LocalDate date = LocalDate.parse(fieldValue);
+            Method setMethod = getSetter(setMethodName, LocalDate.class, objClass);
+            setMethod.invoke(obj, date);
+        }
+    }
+
+    public String getSetMethodName(String fieldName) {
+        return "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+    }
+
+    public Method getSetter(String setterName, Class<?> paramClass, Class<?> objClass) throws NoSuchMethodException {
+        return objClass.getMethod(setterName, paramClass);
     }
 
     public TokenType defineTokenType(String line) {
